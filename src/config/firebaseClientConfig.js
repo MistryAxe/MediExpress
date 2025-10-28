233fd3806fc616bf0bff4e030ffb8c139e9ac3d4
MistryAxe <MISTRYAXE2@GMAIL.COM>
2025-10-28 02:21:03 +0200

    chore(firebase): add firebaseClientConfig mirrored from CareExpress-2 (temporary values)

---
+++ src/config/firebaseClientConfig.js
@@ -0,0 +1,13 @@
+/**
+ * src/config/firebaseClientConfig.js
+ * Replace with your Firebase Web app config when ready.
+ */
+export const firebaseClientConfig = {
+  apiKey: "AIzaSyAZGE0AsMxmLWhCPw4GncNMg9iu-JozX1M",
+  authDomain: "careexpress-d7303.firebaseapp.com",
+  projectId: "careexpress-d7303",
+  storageBucket: "careexpress-d7303.firebasestorage.app",
+  messagingSenderId: "539644664354",
+  appId: "1:539644664354:web:33441d2c485f55ecc6487b",
+  measurementId: "G-6H4GMYHBSJ",
+};
